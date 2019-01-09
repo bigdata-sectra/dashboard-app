0448942649ad4466a883970e9996100a4c5ca946
Codegn <guillermo.nicolas@gmail.com>
2019-01-09 11:09:51 -0300

    outliers detection fix

--- custom-functions/outliers.R
+++ custom-functions/outliers.R
@@ -13,7 +13,7 @@ get_outliers <- function(dt, input_time_grouper, day_type_grouper = 'weekday'){
   dt$floor_minute <- ifelse(minute(dt$floor_date) < 10, paste('0', minute(dt$floor_date), sep=''), minute(dt$floor_date))
   dt$floor_time <- paste(dt$floor_hour, dt$floor_minute, sep=":")
   
-  tt_dt_w_iqr <- dt[,list(q3=quantile(delay, 3/4),q1=quantile(delay, 1/4), count = .N),by=c('name',day_type_grouper,'floor_time')]
+  tt_dt_w_iqr <- dt[,list(q3=quantile(delay, 3/4, type = 1),q1=quantile(delay, 1/4, type = 1), count = .N),by=c('name',day_type_grouper,'floor_time')]
   tt_dt_w_iqr$iqr <- tt_dt_w_iqr$q3 - tt_dt_w_iqr$q1
 
   dt <- dplyr::inner_join(dt, tt_dt_w_iqr, by = c("name",day_type_grouper,"floor_time"))
