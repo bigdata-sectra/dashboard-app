eacfa49d3ea8ff1533e3767c5327a2cd99a1d0b9
Codegn <guillermo.nicolas@gmail.com>
2019-01-10 09:54:48 -0300

    minor update

--- custom-plots/boxplot.R
+++ custom-plots/boxplot.R
@@ -20,26 +20,12 @@ boxplot <- function(dt, input_day_type, input_route, input_date){
                               "<br> total tramo horario :", dt$count[which(dt$name == input_route & dt$day_type == substr(dia_texto, 1, 1))],
                               "<br> Updatetime :", dt$updatetime[which(dt$name == input_route & dt$day_type == substr(dia_texto, 1, 1))]),
                  boxpoints = "all", 
-                 jitter = 0,
+                 jitter = 0.3,
                  pointpos = 0,
                  marker = list(color = c_primary_trace),
                  line = list(color = c_primary_trace)
                  )
       
-    if (sum(dt$outlier[which(dt$name == input_route & dt$date == input_date)]) > 0){
-      p <- add_trace(p,
-                     y = dt$delay[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)], 
-                     x = dt$floor_time[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                     name = paste('outliers dia ', as.character(input_date)),
-                     hovertext = paste("Value :", dt$delay[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                                       "<br> outliers agrupados :", dt$count[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                                       "<br> Updatetime :", dt$updatetime[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)]),
-                     mode = 'markers',
-                     type = 'scatter',
-                     inherit = F,
-                     marker = list(color = c_dots))
-    }
-       
     p <- layout(p,
                 title = paste('Boxplot de tiempos de viaje para los dias', dia_texto), 
                 yaxis=list(title="s/km")) 
@@ -50,7 +36,7 @@ boxplot <- function(dt, input_day_type, input_route, input_date){
                               x= 0.5)) 
     p <- layout(p, 
                 margin = plot_margins) 
-    
+  
   }
   else {
     dia_texto <- lubridate::wday(input_date, label = TRUE, abbr = FALSE)
@@ -62,21 +48,11 @@ boxplot <- function(dt, input_day_type, input_route, input_date){
                               "<br> Updatetime :", dt$updatetime[which(dt$name == input_route & dt$weekday == wday(input_date, week_start = 1))]),
                  boxpoints = "all", 
                  jitter = 0.3,
-                 pointpos = 0
+                 pointpos = 0,
+                 marker = list(color = c_primary_trace),
+                 line = list(color = c_primary_trace)
     )
       
-    if (sum(dt$outlier[which(dt$name == input_route & dt$date == input_date)]) > 0){
-      p <- add_trace(p,
-                     y = dt$delay[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)], 
-                     x = dt$floor_time[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                     name = paste('outliers dia ', as.character(input_date)),
-                     hovertext = paste("Value :", dt$delay[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                                       "<br> N :", dt$count[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)],
-                                       "<br> Updatetime :", dt$updatetime[which(dt$name == input_route & dt$date == input_date & dt$outlier == 1)]),
-                     mode = 'markers',
-                     type = 'scatter',
-                     inherit = F)
-    }
     
     p <- layout(p,
                 title = paste('Boxplot de tiempos de viaje para los dias', dia_texto), 
