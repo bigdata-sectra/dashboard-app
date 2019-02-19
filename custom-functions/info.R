a49aab61c860c1e40ad042888f102343d67a5950
LeoCamus <leocamusc@gmail.com>
2019-02-19 12:11:40 -0300

    minor changes, new data

--- custom-functions/info.R
+++ custom-functions/info.R
@@ -1,22 +1,24 @@
-heatmap_info <- gsub("\n", "","Las pestañas anteriores muestran mapas de calor del tiempo de viaje promedio 
-para el rango de fechas seleccionadas y agrupados según el intervalo de tiempo seleccionado en la parte superior
-de este dashboard. En la primera pestaña se muestra el mapa de calor considerando todos los datos disponibles, 
-es decir, sin filtro de outliers. A su vez, en la segunda pestaña se muestra el mapa de calor quitando 
-de los datos aquellos considerados como outliers según la metodología del rango intercuartil (comentada en la pestaña de información 
+heatmap_info <- gsub("\n", "","Las pestaÃ±as anteriores muestran mapas de calor del tiempo de viaje promedio 
+para el rango de fechas seleccionadas y agrupados segÃºn el intervalo de tiempo seleccionado en la parte superior
+de este dashboard. En la primera pestaÃ±a se muestra el mapa de calor considerando todos los datos disponibles, 
+es decir, sin filtro de outliers. A su vez, en la segunda pestaÃ±a se muestra el mapa de calor quitando 
+de los datos aquellos considerados como outliers segÃºn la metodologÃ­a del rango intercuartil (comentada en la pestaÃ±a de informaciÃ³n 
 del panel inferior izquierdo).")
+Encoding(heatmap_info) <- "UTF-8"
 
-boxplot_info <- gsub("\n", "","Las pestañas de este panel muestran diagramas de caja y bigotes (boxplots) para la categoría 
-correspondiente a la fecha seleccionada, la que viene dada según la forma en la que se decidió agrupar los valores, es decir, si se agrupa 
-según tipo de día (laboral/sábado/domingo) o según día de la semana (L/M/X/J/V/S/D), y para cada intervalo de tiempo seleccionado en la 
-parte superior de este dashboard. Cada intervalo de tiempo muestra un gráfico construido con los datos crudos, el que contiene una caja 
-cuyos límites superior e inferior corresponden a los percentiles 75 (Q3) y 25 (Q1), respectivamente, siendo la barra del medio el percentil 
-50 o mediana (Q2), y un bigote superior e inferior que representan los valores máximo y mínimo, respectivamente. Definiendo el rango 
-intercuartil como IQ = Q3 - Q1, dichos valores máximo y mínimo corresponden al mayor dato que cumple con ser menor o igual a Q3 + 1.5IQ y 
-al menor dato que cumple con ser mayor o igual a Q1 - 1.5IQ, respectivamente. Todos aquellos datos por sobre el valor máximo y por debajo del 
-valor mínimo son considerados como valores atípicos.
-En la primera ventana de este panel se muestran, en color gris, los boxplots para la categoría correspondiente al día seleccionado y para cada 
-intervalo de tiempo seleccionado en la parte superior. A su vez, en color azul se muestra la demora total promedio del día seleccionado, 
-en segundos/kilómetro, para cada intervalo de tiempo. Este gráfico es el mismo que el mostrado en la pestaña 'Agrupados (c/o)' del panel 
-'Datos por Día'. En la segunda pestaña se muestran, en color azul, los boxplots y todos los datos que caen dentro de cada intervalo de tiempo 
-para la categoría correspondiente al día seleccionado.
+boxplot_info <- gsub("\n", "","Las pestaÃ±as de este panel muestran diagramas de caja y bigotes (boxplots) para la categorÃ­a 
+correspondiente a la fecha seleccionada, la que viene dada segÃºn la forma en la que se decidiÃ³ agrupar los valores, es decir, si se agrupa 
+segÃºn tipo de dÃ­a (laboral/sÃ¡bado/domingo) o segÃºn dÃ­a de la semana (L/M/X/J/V/S/D), y para cada intervalo de tiempo seleccionado en la 
+parte superior de este dashboard. Cada intervalo de tiempo muestra un grÃ¡fico construido con los datos crudos, el que contiene una caja 
+cuyos lÃ­mites superior e inferior corresponden a los percentiles 75 (Q3) y 25 (Q1), respectivamente, siendo la barra del medio el percentil 
+50 o mediana (Q2), y un bigote superior e inferior que representan los valores mÃ¡ximo y mÃ­nimo, respectivamente. Definiendo el rango 
+intercuartil como IQ = Q3 - Q1, dichos valores mÃ¡ximo y mÃ­nimo corresponden al mayor dato que cumple con ser menor o igual a Q3 + 1.5IQ y 
+al menor dato que cumple con ser mayor o igual a Q1 - 1.5IQ, respectivamente. Todos aquellos datos por sobre el valor mÃ¡ximo y por debajo del 
+valor mÃ­nimo son considerados como valores atÃ­picos.
+En la primera ventana de este panel se muestran, en color gris, los boxplots para la categorÃ­a correspondiente al dÃ­a seleccionado y para cada 
+intervalo de tiempo seleccionado en la parte superior. A su vez, en color azul se muestra la demora total promedio del dÃ­a seleccionado, 
+en segundos/kilÃ³metro, para cada intervalo de tiempo. Este grÃ¡fico es el mismo que el mostrado en la pestaÃ±a 'Agrupados (c/o)' del panel 
+'Datos por DÃ­a'. En la segunda pestaÃ±a se muestran, en color azul, los boxplots y todos los datos que caen dentro de cada intervalo de tiempo 
+para la categorÃ­a correspondiente al dÃ­a seleccionado.
 ")
+Encoding(boxplot_info) <- "UTF-8"
